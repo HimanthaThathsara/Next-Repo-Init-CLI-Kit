d73edebe07411c8344006b98587e7e60ce415bce
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-07-29 19:52:09 +0530

    Remove the all Version 2 files

--- templates_collection/desgy-tailwind-nextjs-free-v1-2/src/components/Layout/Header/Logo/index.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import Link from "next/link";
-
-const Logo: React.FC = () => {
-  return <Link href="/" className="text-4xl font-semibold">Desgy Solutions</Link>;
-};
-
-export default Logo;
--- templates_collection/desgy-tailwind-nextjs-free-v1-2/src/components/Layout/Header/Navigation/menuData.tsx
+++
@@ -1,9 +0,0 @@
-import { HeaderItem } from "@/types/menu";
-
-export const headerData: HeaderItem[] = [
-  { label: "About Us", href: "#" },
-  { label: "Services", href: "#" },
-  { label: "FAQ", href: "#" },
-  { label: "Blog", href: "#" },
-  { label: "Docs", href: "/documentation" },
-];
